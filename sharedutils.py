2ea0f1f1a6a0e2d0b4d10acc69839b13db2048ba
joshhighet <josh.highet@theta.co.nz>
2021-09-10 10:16:11 +1200

    add charting functions for stats.md

--- sharedutils.py
+++ sharedutils.py
@@ -13,6 +13,9 @@ import requests
 import lxml.html
 import tldextract
 import subprocess
+from datetime import datetime
+from datetime import timedelta
+import plotly.graph_objects as go
 
 sockshost = '127.0.0.1'
 socksport = 9050
@@ -40,6 +43,25 @@ def honk(msg):
     '''critical error logging with termination'''
     logging.critical(msg)
     sys.exit()
+
+def currentmonthstr():
+    months = [
+            "january",
+            "febuary",
+            "march",
+            "april",
+            "may",
+            "june",
+            "july",
+            "august",
+            "september",
+            "october",
+            "november",
+            "december"
+        ]
+    now = (datetime.now())
+    month = (months[now.month])
+    return month
 
 '''
 socks5h:// ensures we route dns requests through the socks proxy
@@ -83,13 +105,13 @@ def socksfetcher(url):
             response = request.text
             return response
         except AttributeError as ae:
-            errlog('sharedutils: ' + 'socks response error - ' + ae)
+            errlog('sharedutils: ' + 'socks response error - ' + str(ae))
             return None
     except requests.exceptions.Timeout as ret:
-        errlog('sharedutils: ' + 'socks request timeout - ' + ret)
+        errlog('sharedutils: ' + 'socks request timeout - ' + str(ret))
         return None
     except requests.exceptions.ConnectionError as rec:
-        errlog('sharedutils: ' + 'socks request connection error - ' + rec)
+        errlog('sharedutils: ' + 'socks request connection error - ' + str(rec))
         return None
 
 def siteschema(location):
@@ -136,8 +158,10 @@ def getsitetitle(html) -> str:
         title = lxml.html.parse(html)
         titletext = title.find(".//title").text
     except AssertionError:
+        stdlog('sharedutils: ' + 'could not fetch site title from source - ' + str(html))
         return None
     except AttributeError:
+        stdlog('sharedutils: ' + 'could not fetch site title from source - ' + str(html))
         return None
     stdlog('sharedutils: ' + 'site title - ' + str(titletext))
     return titletext
@@ -264,3 +288,237 @@ def composecarditem(groupname, victims):
     }
     dbglog('sharedutils: ' + 'composed card item - ' + str(groupitem))
     return groupitem
+
+def postcount():
+    post_count = 1
+    posts = openjson('posts.json')
+    for post in posts:
+        post_count += 1
+    return post_count
+
+def groupcount():
+    groups = openjson('groups.json')
+    return len(groups)
+
+def parsercount():
+    groups = openjson('groups.json')
+    parse_count = 1
+    for group in groups:
+        if group['parser'] is True:
+            parse_count += 1
+    return parse_count
+
+def hostcount():
+    groups = openjson('groups.json')
+    host_count = 0
+    for group in groups:
+        for host in group['locations']:
+            host_count += 1
+    return host_count
+
+def geckocount():
+    groups = openjson('groups.json')
+    gecko_count = 1
+    for group in groups:
+        if group['geckodriver'] is True:
+            gecko_count += 1
+    return gecko_count
+
+def onlinecount():
+    groups = openjson('groups.json')
+    online_count = 0
+    for group in groups:
+        for host in group['locations']:
+            if host['available'] is True:
+                online_count += 1
+    return online_count
+
+def version2count():
+    groups = openjson('groups.json')
+    version2_count = 0
+    for group in groups:
+        for host in group['locations']:
+            if host['version'] == 2:
+                version2_count += 1
+    return version2_count
+
+def version3count():
+    groups = openjson('groups.json')
+    version3_count = 0
+    for group in groups:
+        for host in group['locations']:
+            if host['version'] == 3:
+                version3_count += 1
+    return version3_count
+
+def mounthlypostcount():
+    '''
+    returns the number of posts within the current month
+    '''
+    post_count = 0
+    posts = openjson('posts.json')
+    current_month = datetime.now().month
+    for post in posts:
+        datetime_object = datetime.strptime(post['discovered'], '%Y-%m-%d %H:%M:%S.%f')
+        if datetime_object.month == current_month:
+            post_count += 1
+    return post_count
+
+def postssince(days):
+    '''returns the number of posts within the last x days'''
+    post_count = 0
+    posts = openjson('posts.json')
+    for post in posts:
+        datetime_object = datetime.strptime(post['discovered'], '%Y-%m-%d %H:%M:%S.%f')
+        if datetime_object > datetime.now() - timedelta(days=days):
+            post_count += 1
+    return post_count
+
+def poststhisyear():
+    '''returns the number of posts within the current year'''
+    post_count = 0
+    posts = openjson('posts.json')
+    current_year = datetime.now().year
+    for post in posts:
+        datetime_object = datetime.strptime(post['discovered'], '%Y-%m-%d %H:%M:%S.%f')
+        if datetime_object.year == current_year:
+            post_count += 1
+    return post_count
+
+def postslast24h():
+    '''returns the number of posts within the last 24 hours'''
+    post_count = 0
+    posts = openjson('posts.json')
+    for post in posts:
+        datetime_object = datetime.strptime(post['discovered'], '%Y-%m-%d %H:%M:%S.%f')
+        if datetime_object > datetime.now() - timedelta(hours=24):
+            post_count += 1
+    return post_count
+
+def offlinegroups24h():
+    '''returns the number of groups that have been offline for the last 24 hours'''
+    groups = openjson('groups.json')
+    offline_groups = []
+    for group in groups:
+        for host in group['locations']:
+            if host['available'] is False:
+                if datetime.now() - datetime.strptime(host['lastscrape'], '%Y-%m-%d %H:%M:%S.%f') > timedelta(hours=24):
+                    offline_groups.append(group['name'])
+    return offline_groups
+
+def countcaptchahosts():
+    '''returns a count on the number of groups that have captchas'''
+    groups = openjson('groups.json')
+    captcha_count = 0
+    for group in groups:
+        for host in group['locations']:
+            if host['meta'] == 'has_captcha':
+                captcha_count += 1
+    return captcha_count
+
+'''graphs'''
+
+def groupreportpie():
+    '''
+    create a pie to show the number of posts by each group
+    '''
+    posts = openjson('posts.json')
+    # count the number of posts by group_name within posts.json
+    group_counts = {}
+    for post in posts:
+        if post['group_name'] in group_counts:
+            group_counts[post['group_name']] += 1
+        else:
+            group_counts[post['group_name']] = 1
+    # sort the group_counts - descending
+    sorted_group_counts = sorted(group_counts.items(), key=lambda x: x[1], reverse=True)
+    labels = []
+    values = []
+    for group in sorted_group_counts:
+        labels.append(group[0])
+        values.append(group[1])
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    fig.update_layout(title_text='posts by group')
+    fig.write_image('docs/postsbygroup.png')
+
+def groupreportmonthly():
+    '''chart with plotly the posts by month'''
+    posts = openjson('posts.json')
+    # count the number of posts by group_name within posts.json
+    group_counts = {}
+    for post in posts:
+        if post['group_name'] in group_counts:
+            group_counts[post['group_name']] += 1
+        else:
+            group_counts[post['group_name']] = 1
+    # sort the group_counts - descending
+    sorted_group_counts = sorted(group_counts.items(), key=lambda x: x[1], reverse=True)
+    # create a list of months
+    months = []
+    for post in posts:
+        if post['discovered'].split(' ')[0] not in months:
+            months.append(post['discovered'].split(' ')[0])
+    # create a list of months with counts
+    month_counts = []
+    for month in months:
+        month_counts.append([month, 0])
+    # count the number of posts by group_name within posts.json
+    for post in posts:
+        for month in month_counts:
+            if post['discovered'].split(' ')[0] == month[0]:
+                month[1] += 1
+    # sort the month_counts - descending
+    sorted_month_counts = sorted(month_counts, key=lambda x: x[1], reverse=True)
+    # create a list of months
+    labels = []
+    for month in sorted_month_counts:
+        labels.append(month[0])
+    # create a list of counts
+    values = []
+    for month in sorted_month_counts:
+        values.append(month[1])
+    fig = go.Figure(data=[go.Bar(x=labels, y=values)])
+    fig.update_layout(title_text='posts by month')
+    fig.write_image('docs/postsbymonth.png')
+
+def groupreportyearly():
+    '''
+    chart with plotly the posts by year
+    '''
+    posts = openjson('posts.json')
+    # count the number of posts by group_name within posts.json
+    group_counts = {}
+    for post in posts:
+        if post['group_name'] in group_counts:
+            group_counts[post['group_name']] += 1
+        else:
+            group_counts[post['group_name']] = 1
+    # sort the group_counts - descending
+    sorted_group_counts = sorted(group_counts.items(), key=lambda x: x[1], reverse=True)
+    # create a list of years
+    years = []
+    for post in posts:
+        if post['discovered'].split(' ')[0].split('-')[0] not in years:
+            years.append(post['discovered'].split(' ')[0].split('-')[0])
+    # create a list of years with counts
+    year_counts = []
+    for year in years:
+        year_counts.append([year, 0])
+    # count the number of posts by group_name within posts.json
+    for post in posts:
+        for year in year_counts:
+            if post['discovered'].split(' ')[0].split('-')[0] == year[0]:
+                year[1] += 1
+    # sort the year_counts - descending
+    sorted_year_counts = sorted(year_counts, key=lambda x: x[1], reverse=True)
+    # create a list of years
+    labels = []
+    for year in sorted_year_counts:
+        labels.append(year[0])
+    # create a list of counts
+    values = []
+    for year in sorted_year_counts:
+        values.append(year[1])
+    fig = go.Figure(data=[go.Bar(x=labels, y=values)])
+    fig.update_layout(title_text='posts by year')
+    fig.write_image('docs/postsbyyear.png')
