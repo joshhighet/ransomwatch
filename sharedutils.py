7cdb884d00cf19beed1b120c3271e50697578e14
joshhighet <josh.highet@theta.co.nz>
2021-09-09 19:38:28 +1200

    logging, shell & net utils

--- sharedutils.py
+++ sharedutils.py
@@ -1,24 +1,103 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+'''
+collection of shared modules used throughout ransomwatch
+'''
+import sys
 import json
 import socket
 import codecs
 import random
+import logging
 import requests
 import lxml.html
 import tldextract
 import subprocess
+
+sockshost = '127.0.0.1'
+socksport = 9050
+
+logging.basicConfig(
+    format='%(asctime)s,%(msecs)d %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.INFO
+    )
+
+def stdlog(msg):
+    '''standard infologging'''
+    logging.info(msg)
+
+def dbglog(msg):
+    '''standard debug logging'''
+    logging.debug(msg)
+
+def errlog(msg):
+    '''standard error logging'''
+    logging.error(msg)
+    logging.error(msg)
+
+def honk(msg):
+    '''critical error logging with termination'''
+    logging.critical(msg)
+    sys.exit()
+
 '''
-we use socks5h to route dns through the socks proxy
-this reduces the risk of dns leaks and allows hidden service resolutions
+socks5h:// ensures we route dns requests through the socks proxy
+reduces the risk of dns leaks & allows us to resolve hidden services
 '''
-proxies = {
-    'http':  'socks5h://127.0.0.1:9050',
-    'https': 'socks5h://127.0.0.1:9050'
+
+oproxies = {
+    'http':  'socks5h://' + str(sockshost) + ':' + str(socksport),
+    'https': 'socks5h://' + str(sockshost) + ':' + str(socksport)
 }
 
+def randomagent():
+    '''
+    randomly return a useragent from assets/useragents.txt
+    '''
+    with open('assets/useragents.txt', encoding='utf-8') as uafile:
+        uas = uafile.read().splitlines()
+        uagt = random.choice(uas)
+        dbglog('sharedutils: ' + 'random user agent - ' + str(uagt))
+    return uagt
+
+def headers():
+    '''
+    returns a key:val user agent header for use with the requests library
+    '''
+    headers = {'User-Agent': str(randomagent())}
+    return headers
+
+def socksfetcher(url):
+    '''
+    fetch a url via socks proxy
+    '''
+    try:
+        stdlog('sharedutils: ' + 'starting socks request to ' + str(url))
+        request = requests.get(url, proxies=oproxies, headers=headers())
+        dbglog(
+            'sharedutils: ' + 'socks request - recieved statuscode - ' \
+                + str(request.status_code)
+            )
+        try:
+            response = request.text
+            return response
+        except AttributeError as ae:
+            errlog('sharedutils: ' + 'socks response error - ' + ae)
+            return None
+    except requests.exceptions.Timeout as ret:
+        errlog('sharedutils: ' + 'socks request timeout - ' + ret)
+        return None
+    except requests.exceptions.ConnectionError as rec:
+        errlog('sharedutils: ' + 'socks request connection error - ' + rec)
+        return None
+
 def siteschema(location):
+    '''
+    returns a dict with the site schema
+    '''
     if not location.startswith('http'):
+        dbglog('sharedutils: ' + 'assuming we have been given an fqdn and appending protocol')
         location = 'http://' + location
     schema = {
         'fqdn': getapex(location),
@@ -28,11 +107,112 @@ def siteschema(location):
         'meta': None,
         'available': None,
         'updated': None,
-        'lastscrape': None,
+        'lastscrape': None
     }
+    dbglog('sharedutils: ' + 'schema - ' + str(schema))
     return schema
 
+def runshellcmd(cmd):
+    '''
+    runs a shell command and returns the output
+    '''
+    stdlog('sharedutils: ' + 'running shell command - ' + str(cmd))
+    cmdout = subprocess.run(
+        cmd,
+        shell=True,
+        universal_newlines=True,
+        check=True,
+        stdout=subprocess.PIPE
+        )
+    response = cmdout.stdout.strip().split('\n')
+    return response
+
+def getsitetitle(html) -> str:
+    '''
+    tried to parse out the title of a site from the html
+    '''
+    stdlog('sharedutils: ' + 'getting site title')
+    try:
+        title = lxml.html.parse(html)
+        titletext = title.find(".//title").text
+    except AssertionError:
+        return None
+    except AttributeError:
+        return None
+    stdlog('sharedutils: ' + 'site title - ' + str(titletext))
+    return titletext
+
+def hasprotocol(slug):
+    '''
+    checks if a url begins with http - cheap protocol check before we attampt to fetch a page
+    '''
+    return bool(slug.startswith('http'))
+
+def getapex(slug):
+    '''
+    returns the apex domain for a given webpage/url slug
+    '''
+    stripurl = tldextract.extract(slug)
+    return stripurl.domain + '.' + stripurl.suffix
+
+def striptld(slug):
+    '''
+    strips the tld from a url
+    '''
+    stripurl = tldextract.extract(slug)
+    return stripurl.domain
+
+def getonionversion(slug):
+    '''
+    returns the version of an onion service (v2/v3)
+    https://support.torproject.org/onionservices/v2-deprecation
+    '''
+    version = None
+    stripurl = tldextract.extract(slug)
+    location = stripurl.domain + '.' + stripurl.suffix
+    stdlog('sharedutils: ' + 'checking for onion version - ' + str(location))
+    if len(stripurl.domain) == 16:
+        stdlog('sharedutils: ' + 'v2 onionsite detected')
+        version = 2
+    elif len(stripurl.domain) == 56:
+        stdlog('sharedutils: ' + 'v3 onionsite detected')
+        version = 3
+    return version, location
+
+def openhtml(file):
+    '''
+    opens a file and returns the html
+    '''
+    file = codecs.open(file, 'r', 'utf-8')
+    return file.read()
+
+def openjson(file):
+    '''
+    opens a file and returns the json as a dict
+    '''
+    with open(file, encoding='utf-8') as jsonfile:
+        data = json.load(jsonfile)
+    return data
+
+def checktcp(host, port):
+    '''
+    checks if a tcp port is open - used to check if a socks proxy is available
+    '''
+    stdlog('sharedutils: ' + 'attempting socket connection')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((str(host), int(port)))
+    sock.close()
+    if result == 0:
+        stdlog('sharedutils: ' + 'socket - successful connection')
+        return True
+    stdlog('sharedutils: ' + 'socket - failed connection')
+    return False
+
 def makewebhook(adaptivecard):
+    '''
+    creates a universal webhook for the msft adaptive card
+    https://adaptivecards.io
+    '''
     webhook = {
         'type': 'message',
         'attachments': [
@@ -43,9 +223,13 @@ def makewebhook(adaptivecard):
             }
         ]
     }
+    dbglog('sharedutils: ' + 'composed webhook - ' + str(webhook))
     return webhook
 
 def composecarditem(groupname, victims):
+    '''
+    create the card for the adaptive card webhook
+    '''
     groupitem = {
         'type': 'ColumnSet',
         'columns': [
@@ -78,71 +262,5 @@ def composecarditem(groupname, victims):
             }
         ]
     }
+    dbglog('sharedutils: ' + 'composed card item - ' + str(groupitem))
     return groupitem
-
-def runshellcmd(cmd):
-    cmdout = subprocess.run(
-        cmd, 
-        shell=True, 
-        universal_newlines=True, 
-        check=True, 
-        stdout=subprocess.PIPE
-        )
-    response = cmdout.stdout.strip().split('\n')
-    return response
-
-def headers():
-    agents = open('assets/useragents.txt').read().splitlines()
-    randomagent = random.choice(agents)
-    headers = {'User-Agent': str(randomagent)}
-    return headers
-
-def getsitetitle(html) -> str:
-    try:
-        title = lxml.html.parse(html)
-        titletext = title.find(".//title").text
-    except AssertionError:
-        return None
-    return titletext
-
-def hasprotocol(slug):
-    return slug.startswith('http')
-
-def getapex(slug):
-    stripurl = tldextract.extract(slug)
-    return stripurl.domain + '.' + stripurl.suffix
-
-def striptld(slug):
-    stripurl = tldextract.extract(slug)
-    return stripurl.domain
-
-def gethtml(url):
-    return requests.get(url, headers=headers, proxies=proxies).text
-
-def getonionversion(slug):
-    version = None
-    stripurl = tldextract.extract(slug)
-    location = stripurl.domain + '.' + stripurl.suffix
-    if len(stripurl.domain) == 16:
-        version = 2
-    elif len(stripurl.domain) == 56:
-        version = 3
-    return version, location
-
-def openhtml(file):
-    file = codecs.open(file, 'r', 'utf-8')
-    return file.read()
-
-def openjson(file):
-    with open(file) as f:
-        data = json.load(f)
-    return data
-
-def checktcp(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex(('127.0.0.1',port))
-    sock.close()
-    if result == 0:
-        return True
-    else:
-        return False
