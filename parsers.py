a02547e5c2c88db8f6a9969da5f52896681c8099
josh! <github@joshhighet.com>
2023-12-05 12:15:17 +1300

    add incransom clearnet & backdate fix urlencode parser

--- parsers.py
+++ parsers.py
@@ -1117,8 +1117,9 @@ def knight():
 
 def incransom():
     stdlog('parser: ' + 'incransom')
+    # jq -r '.payload[].title' source/incransom-incback*.html | sed -e 's/%20/ /' || true
     parser = '''
-    jq -r '.payload[].title' source/incransom-incback*.html | sed -e 's/%20/ /' || true
+    jq -r '.payload[].title' source/incransom-incback*.html | perl -MURI::Escape -ne 'print uri_unescape($_)' | sort | uniq || true
     '''
     posts = runshellcmd(parser)
     if len(posts) == 1:
