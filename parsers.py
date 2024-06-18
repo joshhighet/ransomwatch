06c10fe0789b0945245f2fc10d476a2745b085f4
josh! <github@joshhighet.com>
2024-06-18 16:00:14 +1200

    fix dAn0n

--- parsers.py
+++ parsers.py
@@ -1459,8 +1459,9 @@ def embargo():
 def dAn0n():
     stdlog('parser: ' + 'dAn0n')
     # '<h2 class="card-title">'
+    # grep '<h4 class="card-title">' source/dAn0n-2c7nd*.html | cut -d '>' -f 2 | cut -d '<' -f 1 | perl -MURI::Escape -ne 'print uri_unescape($_)' | perl -MURI::Escape -ne 'print uri_unescape($_)'
     parser = '''
-    grep '<h4 class="card-title">' source/dAn0n-2c7nd*.html | cut -d '>' -f 2 | cut -d '<' -f 1 | perl -MURI::Escape -ne 'print uri_unescape($_)' | perl -MURI::Escape -ne 'print uri_unescape($_)'
+    grep '<h6 class="card-title"' source/dAn0n-2c7nd*.html | cut -d '>' -f 2 | cut -d '<' -f 1
     '''
     posts = runshellcmd(parser)
     if len(posts) == 1:
